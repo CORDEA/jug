dcd5229dfaba814c078e78b5b44e1eb94ab4b4f9
Yoshihiro Tanaka <contact@cordea.jp>
2018-02-26 21:37:48 +0900

    add json format

--- src/main/scala/JsonSupport.scala
+++ src/main/scala/JsonSupport.scala
@@ -1,8 +1,9 @@
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import models.{Service, Tag}
+import models.{Error, Service, Tag}
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val serviceFormat: RootJsonFormat[Service] = jsonFormat4(Service)
   implicit val tagFormat: RootJsonFormat[Tag] = jsonFormat2(Tag)
+  implicit val errorFormat: RootJsonFormat[Error] = jsonFormat1(Error)
 }
