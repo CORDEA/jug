620abc47576a4880b4c2751db2e48d57d10da2fa
Yoshihiro Tanaka <contact@cordea.jp>
2018-02-26 21:55:19 +0900

    fix the order

--- src/main/scala/JsonSupport.scala
+++ src/main/scala/JsonSupport.scala
@@ -3,7 +3,7 @@ import models.{Error, Service, Tag}
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val serviceFormat: RootJsonFormat[Service] = jsonFormat4(Service)
   implicit val tagFormat: RootJsonFormat[Tag] = jsonFormat2(Tag)
+  implicit val serviceFormat: RootJsonFormat[Service] = jsonFormat4(Service)
   implicit val errorFormat: RootJsonFormat[Error] = jsonFormat1(Error)
 }
