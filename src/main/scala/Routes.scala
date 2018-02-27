811e62ea261d9f1107ef5533a7574645193b391d
Yoshihiro Tanaka <contact@cordea.jp>
2018-02-27 23:20:49 +0900

    enable basic auth

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,23 +1,39 @@
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.Credentials
 import models.{Error, Service}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 trait Routes extends JsonSupport {
 
   val controller: ServiceController
+  private val realm = "Jug"
+
+  private def authenticator(credentials: Credentials): Future[Option[Unit]] =
+    credentials match {
+      case p@Credentials.Provided(_) =>
+        Future {
+          if (p.verify("")) Some()
+          else None
+        }
+      case _ => Future.successful(None)
+    }
 
   val route: Route =
     pathPrefix("jug") {
       pathPrefix("keys") {
-        pathEnd {
-          post {
-            entity(as[Service]) { service =>
-              onComplete(controller.saveService(service)) {
-                case Success(s) => complete(service)
-                case Failure(it) => complete {
-                  Error(it.getMessage)
+        authenticateBasicAsync(realm, authenticator) { _ =>
+          pathEnd {
+            post {
+              entity(as[Service]) { service =>
+                onComplete(controller.saveService(service)) {
+                  case Success(s) => complete(service)
+                  case Failure(it) => complete {
+                    Error(it.getMessage)
+                  }
                 }
               }
             }
@@ -41,18 +57,18 @@ trait Routes extends JsonSupport {
                   }
                 }
               }
-            }
-        } ~
-          path(Segment) { name =>
-            get {
-              onComplete(controller.getService(name)) {
-                case Success(s) => complete(s)
-                case Failure(it) => complete {
-                  Error(it.getMessage)
+            } ~
+            path(Segment) { name =>
+              get {
+                onComplete(controller.getService(name)) {
+                  case Success(s) => complete(s)
+                  case Failure(it) => complete {
+                    Error(it.getMessage)
+                  }
                 }
               }
             }
-          }
+        }
       }
     }
 }
